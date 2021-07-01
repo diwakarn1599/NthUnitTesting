da2c14b91fa96c4c8a94a371f9a2242aaaacce60
Diwakar <diwakarn1599@gmail.com>
2021-07-01 23:20:28 +0530

    [Diwakar]Initial Commit with Welcome message

---
+++ NthUnitTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NthUnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Nth Unit testing problems");
+        }
+    }
+}
