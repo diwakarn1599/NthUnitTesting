8f297699161e9ebf62dc2a709464a5c19bcae2fd
Diwakar <diwakarn1599@gmail.com>
2021-07-01 23:33:40 +0530

    [Diwakar] usecase 1 - get minimum number of notes in vending machine

--- NthUnitTesting/Program.cs
+++ NthUnitTesting/Program.cs
@@ -7,6 +7,23 @@ namespace NthUnitTesting
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Nth Unit testing problems");
+
+            Console.WriteLine( "Menu\n1.Vending Machine\n2.Exit");
+
+            Console.WriteLine("Enter The option");
+
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    VendingMachine.ReadInput();
+                    break;
+                case 2:
+                    Console.WriteLine("Exited");
+                    break;
+                default:
+                    Console.WriteLine("Enter the correct option");
+                    break;
+            }
         }
     }
 }
